348d290df51d6cb203013d228f74ab9214503561
Gabrielndr <gabriel.2001.2308@gmail.com>
2026-03-19 18:45:37 -0300

    refactor: reorganiza estrutura e2e do Cypress

---
+++ Cypress/support/commands.js
@@ -0,0 +1,2 @@
+// Adicione comandos customizados aqui
+// Exemplo: Cypress.Commands.add('login', (email, password) => { ... })
---
+++ Cypress/support/e2e.js
@@ -0,0 +1,2 @@
+// Support file - carregado antes de cada spec
+import '@4tw/cypress-drag-drop'
